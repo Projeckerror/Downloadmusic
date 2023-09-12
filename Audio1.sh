fcf0a6e69603e62ca710ed1fc261ffcfad50689b
Projeckerror <142165314+Projeckerror@users.noreply.github.com>
2023-09-12 09:24:53 +0700

    Add files via upload

---
+++ Audio1.sh
@@ -0,0 +1,73 @@
+# !/user/bin/bash
+# Selamat Datang Di script Panglima Jatim
+# Jangan lupa subscribe chennel youtube gua
+
+while [ "$else" = "$ulang" ]
+do
+clear
+# warna
+m="\033[1;31m"
+h="\033[1;32m"
+k="\033[1;33m"
+b="\033[1;34m"
+p="\033[1;37m"
+# Tampilan
+sleep 1
+echo
+toilet -f big -F  gay abidin 25
+mpv /storage/emulated/0/Music1
+clear
+sleep 2
+echo "Jangan Lupa Subcribe  dulu chennel YouTube gua"
+
+  xdg-open https://youtube.com/@abidin2582
+
+clear
+echo -e $h
+echo    "       _     _     _ _         ___  _____"  |lolcat
+echo    "      | |   (_)   | (_)       |__ \| ____|" |lolcat
+echo    "  __ _| |__  _  __| |_ _ __      ) | |__"   |lolcat
+echo    " / _  |  _ \| |/ _  | |  -  \   / /|___ \"" |lolcat
+echo    "| (_| | |_) | | (_| | | | | |  / /_ ___) |" |lolcat
+echo    " \__,_|_.__/|_|\__,_|_|_| |_| |____|____/"  |lolcat
+echo -e $p
+echo    "╔═══════════════════════════════════════════════════════════════╗"
+echo -e "║$h                     Author = Panglima Jatim                  $p ║"
+echo -e "║$k                      Time  = Mafia Teknologi                 $p ║"
+echo -e "║$m                     Github = Projeckerror                    $p ║"
+echo    "╚═══════════════════════════════════════════════════════════════╝"
+sleep 1
+echo -e $p
+echo    "╔═══════════════════════════════════════════════════════════════╗"
+echo -e "║$k [1] Download Audio                                            $p║"
+echo -e "║$b [2] Download Vidio                                            $p║"
+echo -e "║$m [3] Keluar                                                    $p║"
+echo    "╚═══════════════════════════════════════════════════════════════╝"
+echo -e $m
+read -p "===> " audio
+
+if [ $audio = 1 ]
+then
+clear
+sleep 2
+  xdg-open https://ytmp3.cc/23d2/
+
+elif [ $audio = 2 ]
+then
+clear
+sleep 2
+  xdg-open https://id.savefrom.net/1-cara-mengunduh-video-youtube-160oX.html
+
+elif [ $audio = 3 ]
+then
+clear
+  echo -e $h "Bay-bay"
+  echo -e $m "sampaai jumpa lagi"
+sleep 3
+exit
+else
+echo -e $h "input salah cuk"
+sleep 2
+clear
+fi
+done
